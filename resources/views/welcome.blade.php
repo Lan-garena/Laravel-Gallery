b3bef2acb062160ca06a3fb9f8fce4778bd03ed1
Lan-garena <rvovna2006@gmail.com>
2024-09-14 13:42:35 +0300

    Secondary commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,33 +7,33 @@
             <div class="col-md-3 gallery-item">
                 <div>
                     <img src="/image.png" alt="" class="img-thumbnail">
-                    <button type="button" class="btn btn-danger mybutton">Danger</button>
-                    <button type="button" class="btn btn-warning mybutton">Warning</button>
-                    <button type="button" class="btn btn-info mybutton">Info</button>
+                    <a href="#" class="btn btn-danger mylink">Delete</a>
+                    <a href="/edit" class="btn btn-warning mylink">Edit</a>
+                    <a href="/show" class="btn btn-info mylink">Show</a>
                 </div>
             </div>
             <div class="col-md-3 gallery-item">
                 <div>
                     <img src="/image.png" alt="" class="img-thumbnail">
-                    <button type="button" class="btn btn-danger mybutton">Danger</button>
-                    <button type="button" class="btn btn-warning mybutton">Warning</button>
-                    <button type="button" class="btn btn-info mybutton">Info</button>
+                    <a href="#" class="btn btn-danger mylink">Delete</a>
+                    <a href="/edit" class="btn btn-warning mylink">Edit</a>
+                    <a href="/show" class="btn btn-info mylink">Show</a>
                 </div>
             </div>
             <div class="col-md-3 gallery-item">
                 <div>
                     <img src="/image.png" alt="" class="img-thumbnail">
-                    <button type="button" class="btn btn-danger mybutton">Danger</button>
-                    <button type="button" class="btn btn-warning mybutton">Warning</button>
-                    <button type="button" class="btn btn-info mybutton">Info</button>
+                    <a href="#" class="btn btn-danger mylink">Delete</a>
+                    <a href="/edit" class="btn btn-warning mylink">Edit</a>
+                    <a href="/show" class="btn btn-info mylink">Show</a>
                 </div>
             </div>
             <div class="col-md-3 gallery-item">
                 <div>
                     <img src="/image.png" alt="" class="img-thumbnail">
-                    <button type="button" class="btn btn-danger mybutton">Danger</button>
-                    <button type="button" class="btn btn-warning mybutton">Warning</button>
-                    <button type="button" class="btn btn-info mybutton">Info</button>
+                    <a href="#" class="btn btn-danger mylink">Delete</a>
+                    <a href="/edit" class="btn btn-warning mylink">Edit</a>
+                    <a href="/show" class="btn btn-info mylink">Show</a>
                 </div>
             </div>
         </div>
